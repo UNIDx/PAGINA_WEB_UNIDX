23a3f73de93b8ed1f673d2a09c5d4763cffa8a46
jonas-bm <jonas.barri16@gmail.com>
2026-05-21 16:52:38 -0500

    OK

--- app/page.tsx
+++ app/page.tsx
@@ -38,4 +38,4 @@ export default function Home() {
 
 
 
-//añaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAaaaaaaaaaya
+//añaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAaaaaaaa
